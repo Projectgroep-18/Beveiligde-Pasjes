d6af12ab0e2f068ee870f0d88a61324b266a0f56
JeroenWeener <jweener1996@gmail.com>
2014-10-10 13:13:55 +0200

    Aanpassingen Arduinocode

--- Moduleproject.ino
+++ Moduleproject.ino
@@ -1,11 +1,17 @@
 void setup() {
   // Open serial connection
   Serial.begin(9600);
-  pinMode(13, OUTPUT);
-  Serial.write('1');
+  pinMode(5V , OUTPUT);
+  pinMode(GND , OUTPUT);
+  pinMode(13 , OUTPUT);
+  pinMode(12 , OUTPUT);
+  pinMode(11 , OUTPUT);
+  pinMode(10 , OUTPUT);
+  pinMode(9 , OUTPUT);
+
 }
 
-void loop() {
+void main() {
   if (Serial.available() > 0) {  // als er data beschikbaar is...
   
 

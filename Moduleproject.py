33413766802499516d6dae381743ae544c576bef
JeroenWeener <jweener1996@gmail.com>
2014-10-17 13:36:01 +0200

    ...

--- Moduleproject.py
+++ Moduleproject.py
@@ -1,17 +1,5 @@
 import serial
 
-def COMPOORT = "COM1"
-
-## Boolean die aangeeft of de Arduino verbonden is of niet
-connected = False
-
-## Opent de poort waar de Arduino mee verbonden is
-ser = serial.Serial(COMPOORT, 9600)
-
-## Een loop die checkt of de Arduino verbinding heeft
-while not connected:
-	serin = ser.read()
-	connected = True
-	
-## sluit de poort en eindigt het programma
-ser.close()
+ser = serial.Serial('/dev/tty.usbserial'. 9600)
+while True:
+	print ser.readline()

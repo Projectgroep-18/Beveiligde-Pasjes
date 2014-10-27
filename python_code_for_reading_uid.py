b217dab40990f706c3b925676f1f8b7e31905d3a
JeroenWeener <jweener1996@gmail.com>
2014-10-27 16:20:10 +0100

    ...

--- python_code_for_reading_uid.py
+++ python_code_for_reading_uid.py
@@ -1,15 +1,26 @@
 import serial
-ser = serial.Serial(3)
+import sqlite3
+conn = sqlite3.connect('data.db')
+from random import randint
 
-#ser.print("password")
+c = conn.cursor()
+
+def check(userid):
+	c.execute("""SELECT UID from persoon WHERE UID = %i """ % userid)
+	persoon = c.fetchall()
+	if persoon:
+		return True
+	else:
+		return False
+
+ser = serial.Serial(3)
+#key = randint(0, 100)
+
+#ser.write(key)
 
 s = ser.read(10)
 print( s )
+userid = int.from_bytes(s, byteorder='big')
 
-#c.execute ('''SELECT (card id) FROM persoon WHERE (card id) == s''')
-
-if s == b'3529442660':
-	Door = True
-else:
-	Door = False
+Door = check(userid)
 print( Door )

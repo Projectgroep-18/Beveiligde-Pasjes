69fc5955fe9777b0f967fee992ede18576e6ba03
DaTurtle <janwillemnijhuis@hotmail.com>
2014-10-28 11:53:38 +0100

    3 inputs die geprint worden als button gedrukt is

--- Gui.py
+++ Gui.py
@@ -3,18 +3,31 @@ from tkinter import *
 def useless():
     print("I lied, i actually do something! HUEHUEHUE")
 
+def printVars():
+    print(entryName.get())
+    print(entryUID.get())
+    print(entryRights.get())
+
 root = Tk()
 root.title("Dit is de titel")
-#root.geometry("500x500")
+inputVar1 = ""
+inputVar2 = ""
+inputVar3 = ""
 
-image = PhotoImage(file="dickbutt.png")
+buttonSearch = Button(root, text="Search", command=printVars, height=3)
+entryName = Entry(root, textvariable=inputVar1)
+labelName = Label(root, text="Name")
+entryUID = Entry(root, textvariable=inputVar2)
+labelUID = Label(root, text="User ID")
+entryRights = Entry(root, textvariable=inputVar3)
+labelRights = Label(root, text="Rights")
 
-button1 = Button(root, text="Knop 1", command=useless)
-button2 = Button(root, text="Knop 2", command=useless)
-button3 = Button(root, image=image, command=useless, bd=5)
-
-button1.grid(row=0, column=0)
-button2.grid(row=0, column=1)
-button3.grid(row=1, columnspan=2)
+labelName.grid(row=0, column=0, sticky=E)
+entryName.grid(row=0, column=1)
+labelUID.grid(row=1, column=0, sticky=E)
+entryUID.grid(row=1, column=1)
+labelRights.grid(row=2, column=0, sticky=E)
+entryRights.grid(row=2, column=1)
+buttonSearch.grid(row=0, column=2, rowspan=3)
 
 root.mainloop()

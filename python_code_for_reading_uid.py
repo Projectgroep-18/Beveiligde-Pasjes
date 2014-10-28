eb4cdfe362b49a84c6d955dab675ee998b328e09
JeroenWeener <j.weener@student.utwente.nl>
2014-10-28 17:07:29 +0100

    ?

--- python_code_for_reading_uid.py
+++ python_code_for_reading_uid.py
@@ -6,22 +6,29 @@ from random import randint
 
 c = conn.cursor()
 
+def check(userid):
 COMPOORT = int(input("De Arduino is aangesloten op COM-poort "))					#De user wordt gevraagd om de COM-poort te specificeren die de Arduino gebruikt
 
 def check(userid):																	#Checkt of de ontvangen UID voorkomt in onze database
 	c.execute("""SELECT UID from persoon WHERE UID = %i """ % userid)
 	persoon = c.fetchall()
 	if persoon:
-		return True
+@@ -13,14 +16,23 @@ def check(userid):
 	else:
 		return False
 
+ser = serial.Serial(3)
+#key = randint(0, 100)
 													#Hier wordt de eerder gevraagte COM-poort gebruikt
 #key = randint(0, 100)																#Genereerd een 'random' getal tussen de 0 en de 100 als key
 
 #ser.write(key)																		#Stuurt de key naar de Arduino
 
+#ser.write(key)
 
+s = ser.read(10)
+print( s )
+userid = int.from_bytes(s, byteorder='big')
 while 1==1:
 	print( 'Scan pasje a.u.b.' )
 	ser = serial.Serial(COMPOORT - 1)
@@ -32,6 +39,9 @@ while 1==1:
 	print( Door )
 	ser.close()
 
+Door = check(userid)
+print( Door )
+ No newline at end of file
 	if Door:																			#Wanneer de deur succesvol geopend is, sluit deze weer na 5 seconden
 		time.sleep(5)
 		Door = False

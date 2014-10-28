ab2cb3627bb9e36a491ad5a674f21831c341253f
JeroenWeener <j.weener@student.utwente.nl>
2014-10-28 10:38:25 +0100

    Verbetering code, werkt echter nog niet optimaal

--- python_code_for_reading_uid.py
+++ python_code_for_reading_uid.py
@@ -1,10 +1,12 @@
 import serial
-import sqlite3
 import time
+import sqlite3
 conn = sqlite3.connect('data.db')
 from random import randint
 
 c = conn.cursor()
+
+COMPOORT = int(input("De Arduino is aangesloten op COM-poort "))					#De user wordt gevraagd om de COM-poort te specificeren die de Arduino gebruikt
 
 def check(userid):																	#Checkt of de ontvangen UID voorkomt in onze database
 	c.execute("""SELECT UID from persoon WHERE UID = %i """ % userid)
@@ -14,19 +16,21 @@ def check(userid):																	#Checkt of de ontvangen UID voorkomt in onze 
 	else:
 		return False
 
-ser = serial.Serial(3)
+ser = serial.Serial(COMPOORT - 1)													#Hier wordt de eerder gevraagte COM-poort gebruikt
 #key = randint(0, 100)																#Genereerd een 'random' getal tussen de 0 en de 100 als key
 
 #ser.write(key)																		#Stuurt de key naar de Arduino
 
-s = ser.read(10)
-#print( s )
-userid = int.from_bytes(s, byteorder='big')
 
-Door = check(userid)
-print( Door )
-
-if Door:																			#Wanneer de deur succesvol geopend is, sluit deze weer na 5 seconden
-	time.sleep(5)
-	Door = False
+while True:
+	print( 'Scan pasje a.u.b.' )
+	s = ser.read(10)
+	#print( s )																			#Hiermee kan eventueel de UID worden weergeven
+	userid = int.from_bytes(s, byteorder='big')
+	Door = check(userid)
 	print( Door )
+
+	if Door:																			#Wanneer de deur succesvol geopend is, sluit deze weer na 5 seconden
+		time.sleep(5)
+		Door = False
+		print( Door )

3672fa6918d23a9d27ef5acfccd48a6791337162
d-cai95 <dennis.c95@gmail.com>
2014-10-28 15:10:22 +0100

    numerous fixes
    paar dingen gestroomlijnd, verbeterd.

--- Gui.py
+++ Gui.py
@@ -2,10 +2,6 @@ from tkinter import *
 import tkinter.messagebox
 
 
-def useless():
-    print("I lied, i actually do something! HUEHUEHUE")
-
-
 def string_int(string):
     try:
         int(string)
@@ -14,29 +10,30 @@ def string_int(string):
         return -1
 
 
-def print_vars1():
-    var1 = (entryName1.get())
-    var2 = (entryUID1.get())
+def search():
+    var1 = entryName1.get()
+    var2 = entryCID1.get()
     if var2 != "":
         var2 = string_int(var2)
-    var3 = (entryRights1.get())
+    var3 = entryRights1.get()
     if var2 != -1:
-        print(var1, var2, var3)
+        if var1 != "":
+            print(var1, var2, var3)
     else:
-        tkinter.messagebox.showerror("Wrong Input", "User ID must be an integer")
+        tkinter.messagebox.showerror("Wrong Input", "Card ID must be an integer")
 
 
 def print_vars2():
-    var1 = (entryName2.get())
-    var2 = (entryUID2.get())
+    var1 = entryName2.get()
+    var2 = entryCID2.get()
     if var2 != "":
         var2 = string_int(var2)
-    var3 = (entryRights2.get())
+    var3 = entryRights2.get()
     if var1 != "" and var2 != "" and var3 != "":
         if var2 != -1:
             print(var1, var2, var3)
         else:
-            tkinter.messagebox.showerror("Wrong Input", "User ID must be an integer")
+            tkinter.messagebox.showerror("Wrong Input", "Card ID must be an integer")
     else:
         tkinter.messagebox.showerror("Wrong Input", "Some fields are empty")
 
@@ -50,27 +47,27 @@ inputVar4 = ""                                                              # In
 inputVar5 = ""                                                              # Input UID
 inputVar6 = ""                                                              # Input rights
 
-buttonSearch = Button(root, text="Search", command=print_vars1, height=3)
+buttonSearch = Button(root, text="Search", command=search, height=3)
 entryName1 = Entry(root, textvariable=inputVar1)
 labelName1 = Label(root, text="Name")
-entryUID1 = Entry(root, textvariable=inputVar2)
-labelUID1 = Label(root, text="User ID")
+entryCID1 = Entry(root, textvariable=inputVar2)
+labelCID1 = Label(root, text="Card ID")
 entryRights1 = Entry(root, textvariable=inputVar3)
 labelRights1 = Label(root, text="Rights")
 
 buttonEnter = Button(root, text="Enter", command=print_vars2, height=3)
 entryName2 = Entry(root, textvariable=inputVar4)
 labelName2 = Label(root, text="Name")
-entryUID2 = Entry(root, textvariable=inputVar5)
-labelUID2 = Label(root, text="User ID")
+entryCID2 = Entry(root, textvariable=inputVar5)
+labelCID2 = Label(root, text="Card ID")
 entryRights2 = Entry(root, textvariable=inputVar6)
 labelRights2 = Label(root, text="Rights")
 labelWhite = Label(root, text="")
 
 labelName1.grid(row=0, column=0, sticky=E)
 entryName1.grid(row=0, column=1)
-labelUID1.grid(row=1, column=0, sticky=E)
-entryUID1.grid(row=1, column=1)
+labelCID1.grid(row=1, column=0, sticky=E)
+entryCID1.grid(row=1, column=1)
 labelRights1.grid(row=2, column=0, sticky=E)
 entryRights1.grid(row=2, column=1)
 buttonSearch.grid(row=0, column=2, rowspan=3)
@@ -78,12 +75,11 @@ buttonSearch.grid(row=0, column=2, rowspan=3)
 labelWhite.grid(row=3)
 labelName2.grid(row=4, column=0, sticky=E)
 entryName2.grid(row=4, column=1)
-labelUID2.grid(row=5, column=0, sticky=E)
-entryUID2.grid(row=5, column=1)
+labelCID2.grid(row=5, column=0, sticky=E)
+entryCID2.grid(row=5, column=1)
 labelRights2.grid(row=6, column=0, sticky=E)
 entryRights2.grid(row=6, column=1)
 buttonEnter.grid(row=4, column=2, rowspan=3)
-
 
 
 root.mainloop()

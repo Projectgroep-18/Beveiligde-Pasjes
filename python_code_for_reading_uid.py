98f889d5eb23e084d4c9bdb5c1d33400e59d2503
JeroenWeener <j.weener@student.utwente.nl>
2014-10-28 10:03:48 +0100

    Deur gaat nu vanzelf weer dicht

--- python_code_for_reading_uid.py
+++ python_code_for_reading_uid.py
@@ -1,11 +1,12 @@
 import serial
 import sqlite3
+import time
 conn = sqlite3.connect('data.db')
 from random import randint
 
 c = conn.cursor()
 
-def check(userid):
+def check(userid):																	#Checkt of de ontvangen UID voorkomt in onze database
 	c.execute("""SELECT UID from persoon WHERE UID = %i """ % userid)
 	persoon = c.fetchall()
 	if persoon:
@@ -14,13 +15,18 @@ def check(userid):
 		return False
 
 ser = serial.Serial(3)
-#key = randint(0, 100)
+#key = randint(0, 100)																#Genereerd een 'random' getal tussen de 0 en de 100 als key
 
-#ser.write(key)
+#ser.write(key)																		#Stuurt de key naar de Arduino
 
 s = ser.read(10)
-print( s )
+#print( s )
 userid = int.from_bytes(s, byteorder='big')
 
 Door = check(userid)
 print( Door )
+
+if Door:																			#Wanneer de deur succesvol geopend is, sluit deze weer na 5 seconden
+	time.sleep(5)
+	Door = False
+	print( Door )

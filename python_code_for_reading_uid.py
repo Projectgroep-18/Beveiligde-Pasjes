39a3bcddefb7c859c169da81a98280a19ced2349
JeroenWeener <j.weener@student.utwente.nl>
2014-10-29 12:09:28 +0100

    .

--- python_code_for_reading_uid.py
+++ python_code_for_reading_uid.py
@@ -7,17 +7,30 @@ from random import randint
 c = conn.cursor()
 
 COMPOORT = int(input("De Arduino is aangesloten op COM-poort "))
-TID = int(input("Ik wil graag toegang tot deur "))
 
-def check(userid, TID):
-	c.execute("""SELECT UID from persoon WHERE UID = %i """ % userid)
-	persoon = c.fetchall()
-	if persoon:
-		return True
-	else:
-		return False
+def check(CID, TID):
+    c.execute("""SELECT Rechten from persoon WHERE CID = %i AND Access='Aan' """ % CID)
+    persoon = c.fetchall()
+    if persoon:
+        persoon = persoon[0][0]
+    print("persoon =", persoon)
+    c.execute("""SELECT Rechten from terminal WHERE TID = %i""" % TID)
+    terminal = c.fetchall()
+    if terminal:
+        terminal = terminal[0][0]
+    print("terminal =", terminal)
+    if not persoon:
+        print('Deze Card ID staat niet in de database.')
+        return False
+    elif persoon >= terminal:
+        print('Open deur!')
+        return True
+    else:
+        print('Deze gebruiker heeft geen toegang tot deze deur.')
+        return False
 
 while True:
+	TID = int(input("Ik wil graag toegang tot deur "))
 	#key = randint(1, 100)
 	print('Scan pasje a.u.b.')
 	ser = serial.Serial(COMPOORT - 1)
@@ -26,6 +39,7 @@ while True:
 	s = ser.read(10)
 	userid = int.from_bytes(s, byteorder='big')
 	Door = check(userid, TID)
+	print(userid)
 	print(Door)
 	ser.close()
 

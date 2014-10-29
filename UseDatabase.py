17366cdbc5f98820147126ec54cced74ab93d6ca
JLedelay <j.ledelay@student.utwente.nl>
2014-10-29 11:46:02 +0100

    Rechten is nu een integer
    In UseDatabase is rechten dus nu ook een integer, en ik heb ook wat
    minor fixes uitgevoerd om onnodige errors te voorkomen.

--- UseDatabase.py
+++ UseDatabase.py
@@ -8,28 +8,37 @@ c = conn.cursor()
 # We gaan uit van byteorder = big
 userid = 246452191867917525661493  # int.from_bytes(b'3529442660', byteorder='big')
 
+
 def check(CID, TID=1):
     c.execute("""SELECT Rechten from persoon WHERE CID = %i AND Access='Aan' """ % CID)
-    persoon = c.fetchall()[0][0]
+    persoon = c.fetchall()
+    if persoon:
+        persoon = persoon[0][0]
     print("persoon =", persoon)
     c.execute("""SELECT Rechten from terminal WHERE TID = %i""" % TID)
-    terminal = c.fetchall()[0][0]
+    terminal = c.fetchall()
+    if terminal:
+        terminal = terminal[0][0]
     print("terminal =", terminal)
-    if persoon == terminal:
-        print('Open deur!')
-        return True
-    elif persoon:
-        print('Deze gebruiker heeft geen toegang tot deze deur')
-        return False
-    else:
+    if not persoon:
         print('Deze Card ID staat niet in de database.')
         return False
+    elif persoon >= terminal:
+        print('Open deur!')
+        return True
+    else:
+        print('Deze gebruiker heeft geen toegang tot deze deur.')
+        return False
+
+
 #functie om nieuwe users toe te voegen
 def add(CID, Naam, Rechten):
     c.execute("""SELECT COUNT(UID) from persoon""")
     UID = c.fetchall()[0][0]+1
-    c.execute("""INSERT INTO persoon VALUES (%i, %i, '%s', '%s','Uit') """ % (UID, CID, Naam, Rechten))
+    c.execute("""INSERT INTO persoon VALUES (%i, %i, '%s', '%s', 'Aan') """ % (UID, CID, Naam, Rechten))
+    conn.commit()
     print(Naam, ' toegevoegd')
+
 
 # functie om users te verwijderen
 def delete(CID):
@@ -43,9 +52,11 @@ def delete(CID):
         lastUID = c.fetchall()[0][0]
         c.execute("""DELETE from persoon WHERE CID=%i """ % CID)
         c.execute("""UPDATE persoon SET UID = %i WHERE UID = %i""" % (UID, lastUID))
-        print(Naam, ' verwijderd')
+        print(Naam, 'verwijderd')
     else:
         print('Invalid Card ID')
+    conn.commit()
+
 
 def activeer(CID):
     c.execute("""SELECT Naam FROM persoon WHERE CID = %i AND Access = 'Uit'""" % CID)
@@ -53,21 +64,25 @@ def activeer(CID):
     if NaamTupel:
         c.execute("""UPDATE persoon SET Access = 'Aan' WHERE CID = %i """ % CID)
         Naam = NaamTupel[0][0]
-        print('Het pasje van ', Naam, ' staat nu aan!')
+        print('Het pasje van', Naam, 'staat nu aan!')
         return Naam
     else:
         print('Card ID bestaat niet of staat al aan.')
+    conn.commit()
+
 
 def deactiveer(CID):
     c.execute("""SELECT Naam FROM persoon WHERE CID = %i AND Access = 'Aan'""" % CID)
     NaamTupel = c.fetchall()
     if NaamTupel:
-        c.execute("""UPDATE persoon SET Access = 'Uit' WHERE CID = %i """ % CID)
+        c.execute("""UPDATE persoon SET Access = 'Uit' WHERE CID = %i""" % CID)
         Naam = NaamTupel[0][0]
-        print('Het pasje van ', Naam, ' staat nu uit!')
+        print('Het pasje van', Naam, 'staat nu uit!')
         return Naam
     else:
         print('Card ID bestaat niet of staat al uit.')
+    conn.commit()
+
 
 def searchNaam(Naam):
     c.execute("""SELECT * from persoon WHERE Naam = '%s'""" % Naam)
@@ -85,6 +100,7 @@ def searchNaam(Naam):
     else:
         print('not found.')
         return False
+
 
 def searchCID(CID):
     c.execute("""SELECT * from persoon WHERE CID = %i""" % CID)
@@ -124,10 +140,13 @@ def searchRechten(Rechten):
 
 #searchRechten('Eigenaar')
 activeer(246452191867917525661493)
-check(246452191867917525661493)
-add(123123189371937128937912, 'Jan Jaap', 'Eggeneer')
-# delete(241821987043432866395696)
-add(12, 'Piet Pietersen', 'KIP')
+add(123123189371937128937912, 'Jan Jaap', 3)
+delete(241821987043432866395696)
+add(12, 'Piet Pietersen', -1)
+c.execute("""SELECT * from persoon where cid = 12""")
+persoon = c.fetchall()
+print(persoon)
+check(12)
 c.execute("""SELECT * from persoon""")
 persoon = (c.fetchall())
 print(persoon)

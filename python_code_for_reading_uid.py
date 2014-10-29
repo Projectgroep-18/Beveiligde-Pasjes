a182a667808550a19b3ae43adbcc00d3aa05830e
JeroenWeener <j.weener@student.utwente.nl>
2014-10-29 14:35:08 +0100

    fdsfdsf

--- python_code_for_reading_uid.py
+++ python_code_for_reading_uid.py
@@ -3,11 +3,31 @@ import time
 import sqlite3
 conn = sqlite3.connect('data.db')
 from random import randint
-import UseDatabase
 
 c = conn.cursor()
 
 COMPOORT = int(input("De Arduino is aangesloten op COM-poort "))
+
+def check(CID, TID):
+    c.execute("""SELECT Rechten from persoon WHERE CID = %i AND Access='Aan' """ % CID)
+    persoon = c.fetchall()
+    if persoon:
+        persoon = persoon[0][0]
+    print("persoon =", persoon)
+    c.execute("""SELECT Rechten from terminal WHERE TID = %i""" % TID)
+    terminal = c.fetchall()
+    if terminal:
+        terminal = terminal[0][0]
+    print("terminal =", terminal)
+    if not persoon:
+        print('Deze Card ID staat niet in de database.')
+        return False
+    elif persoon >= terminal:
+        print('Open deur!')
+        return True
+    else:
+        print('Deze gebruiker heeft geen toegang tot deze deur.')
+        return False
 		
 while True:
 	temp = []
@@ -26,7 +46,7 @@ while True:
 	#ser.write(bytes([key]))
 	s = ser.read(10)
 	userid = int.from_bytes(s, byteorder='big')
-	Door = UseDatabase.check(userid, TID)
+	Door = check(userid, TID)
 	print(Door)
 	ser.close()
 
